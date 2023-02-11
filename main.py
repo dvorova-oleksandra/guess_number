1ff8ec56cf5e33fd5cff716ebdba6a6dccc58def
Oleksandra Dvorova <sashadvorovay@gmail.com>
2023-02-11 18:08:59 +0200

    Create comments

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import random
 def start_text():
+    '''Output gretting text, and choose difficulty'''
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
     return difficulty
 
 def create_number():
+    '''Create number, that you must guess'''
     number_for_guessing = random.randint(1, 100)
     return number_for_guessing
 
 def checking_number(guess_number):
+    '''Checking input number to equal guess number'''
     number = int(input("Make a guess: "))
     flag = False
     if number > guess_number:
@@ -22,6 +25,7 @@ def checking_number(guess_number):
     return flag
 
 def loop_for_guessing(difficulty, guess_number):
+    '''Loop for guess number'''
     i = difficulty
     while i > 0:
         print(f"You have {i} attempts remaining to guess the number")
@@ -33,6 +37,7 @@ def loop_for_guessing(difficulty, guess_number):
         i -= 1
 
 def choose_difficulty(difficulty):
+    '''Choose difficulty (hard or easy)'''
     number = create_number()
     if difficulty == 'hard':
         loop_for_guessing(5, number)
@@ -41,8 +46,8 @@ def choose_difficulty(difficulty):
 
 
 
-
 def main():
+    '''run program'''
     difficulty = start_text()
     choose_difficulty(difficulty)
 
